6e2a9a92fd6bfad57310288588f7a3b14edb5d04
Sami Safatli <sami.safatli@yduqs.com.br>
2023-12-20 20:06:24 -0300

    Add Contact page component

--- src/Pages/ContactPage/ContactPage.tsx
+++ src/Pages/ContactPage/ContactPage.tsx
@@ -1,9 +1,74 @@
+import { useState } from 'react';
+import './ContactPage.css'
+
 function ContactPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e: { target: { name: any; value: any; }; }) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <div className="container">
-        <h1>ContactPage</h1>
-      </div> 
+    <div className='contact_page_container'>
+      <div className="contact-page">
+      <h1>Contact Me</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="subject">Subject:</label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          value={formData.subject}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* Additional contact information and social media links can be added here */}
+    </div>
+
     </div>
   )
 }
